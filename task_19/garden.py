1121eaa6547887c24a739fd5c8acc52e2b0981e7
Barney Fairnington <104977435+bfairnington@users.noreply.github.com>
2024-03-30 22:27:46 +0000

    Update garden.py
    Descriptions and changes made to make code more readable

--- task_19/garden.py
+++ task_19/garden.py
@@ -36,41 +36,72 @@ looked up. For each entity answer the following questions:
 
 """
 
+# Create garden path sentence list
 gardenpathSentences = ["When Fred eats food gets thrown.", "We painted the wall with cracks."]
 
+# Append additional garden path sentences
 gardenpathSentences.append("Mary gave the child a Band-Aid.")
 gardenpathSentences.append("That Jill is never here hurts.")                           
 gardenpathSentences.append("The cotton clothing is made of grows in Mississippi.")
 
+# Import library
 import spacy
 
+# Define nlp function
 nlp = spacy.load('en_core_web_sm')
 
 
+# Inspect words identified as tokens in each sentence in the list
+
+# Loop through all sentences
 for i in range(len(gardenpathSentences)):
+    # Print sentence number
     print(f"\nSentence {i+1})")
 
+    # Loop through each word in each sentence
     for token in nlp(gardenpathSentences[i]):
+
+        # Exclude punction and stop words
         if not token.is_punct and not token.is_stop:
+            # Print token, orth_ and orth
             print(token, token.orth_, token.orth)
 
+
+# Perform named entity recognition 
+
+# Loop through all sentences
 for i in range(len(gardenpathSentences)):
+    # Print sentence number
     print(f"\nSentence {i+1})")
+
+    # Loop through each word in each sentence
     for token in nlp(gardenpathSentences[i-1]).ents:
+        # Print token, label_ and label
         print(token, token.label_, token.label)
 
 print(f"\nAs can be seen the .ents .label_ method is only able to categorise proper nouns (names) in the list of garden path sentences.")
 
+
+# Use spacy to explain the categorisations applied
+
+# Create empty list
 label_list = [] 
 
+# Loop through all sentences
 for i in range(len(gardenpathSentences)):
+
+    # Loop through each word in each sentence
     for token in nlp(gardenpathSentences[i-1]).ents:
+
+        # Create list of all labels identified
         label_list.append(token.label_)
 
+# For each label print the spacy label_ and its spacy explanation
 for label in range(len(label_list)):
     print()
     print(label_list[label] + " = " + spacy.explain(label_list[label]))
 
+# Answer the final two questions
 print("\nWhat was the entity and its explanation that you looked up?")
 print(label_list[0] + " & " + label_list[1])
 print()
